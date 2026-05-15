5bcf4a08c8e778cb55d08b6ea81446ef173a9a51
Leandro Lasala <leandro.lasala14@gmail.com>
2026-05-15 20:30:26 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HelloWorld from './components/Formulario.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Mi primera app en vue</h1>
+    <button @click="contador++">
+    Clicks : {{contador}}
+    </button>
+  </div> 
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+</script>
+
+<style scoped>
+ button{
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+ }
+
+</style>
+
